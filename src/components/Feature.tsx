d63cce96942d082bc5b2004f4852437f38fe46f2
Rachman Chavik <39490+rchavik@users.noreply.github.com>
2020-10-11 14:03:57 +0700

    Port Feature component to typescript

---
+++ src/components/Feature.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+declare interface FeatureProps {
+  color: string;
+  icon?: string;
+  title: string;
+  text: string;
+  imgPath?: string | undefined;
+}
+export const Feature = (props: FeatureProps) => {
+  const { color, imgPath, text, title } = props;
+  return (
+    <div className="feature text-center">
+      { imgPath
+          ? <img alt='' src={ imgPath } className='img-fluid mb-5' style={{ maxHeight: '120px' }}/>
+          : null
+      }
+      <h5 className={`text-${ color }`}>{ title }</h5>
+      <p className="lead">{ text }</p>
+    </div>
+  );
+};
+
+export default Feature;
