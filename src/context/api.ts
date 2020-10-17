019e9c88792397b5a42f630ef90287d28f48ddca
Rachman Chavik <39490+rchavik@users.noreply.github.com>
2020-10-17 11:39:00 +0700

    Re-add axios caching
    It seems that `useCache` of react-use-api is intended for configuring
    ssr cache usage. This adds caching option when the app has been rendered
    on the client.

--- src/context/api.ts
+++ src/context/api.ts
@@ -1,7 +1,17 @@
-import Axios, { AxiosInstance } from 'axios';
+import Axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { setupCache } from 'axios-cache-adapter';
 import config from 'config';
 import Jsona, { SwitchCaseJsonMapper, SwitchCaseModelMapper } from 'jsona';
+import qs from 'qs';
 import { CustomContext } from 'react-use-api/build/typings';
+
+const cache = setupCache({
+  maxAge: 5 * 60 * 1000,
+  exclude: {
+    query: false,
+  },
+  key: (req: any) => req.url + qs.stringify(req.params),
+});
 
 export const dataFormatter = new Jsona({
   modelPropertiesMapper: new SwitchCaseModelMapper(),
@@ -18,12 +28,13 @@ export const makeApiContext = (axios: AxiosInstance): CustomContext => {
 }
 
 export const makeAxios = () => {
-  let axiosConfig = {
+  let axiosConfig: AxiosRequestConfig = {
     baseURL: config.api.baseUrl,
     headers: {
       'Accept': 'application/vnd.api+json',
       'X-ApiToken': config.api.token,
     },
+    adapter: cache.adapter,
   }
   return Axios.create(axiosConfig);
 }
